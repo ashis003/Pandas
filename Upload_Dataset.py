eb5edc3702f9541ebc15847312de48b03d8a0456
Ashis Kumar Singh <ashisaks03@gmail.com>
2023-03-24 07:32:10 +0530

    first commit

--- Upload_Dataset.py
+++ Upload_Dataset.py
@@ -1,3 +1,11 @@
 import pandas as pd
 df=pd.read_csv("insurance.csv")
 print(df)
+#Maximum value of age
+print(df['age'].max())
+print(df['age'])
+#Print the gender whose age is maximum
+print(df['sex'][df['age']==14])
+#Setting 0 to missing values
+df.fillna(0,inplace=True)
+print(df['age'].mean())
